9b478ca3cdb59417e2ffbb8fcf899cb9d2213a96
itsmrajeshkumar <itsmrajeshkumar@gmail.com>
2024-06-10 08:06:21 +0530

    diff operations learned on 10 jun

---
+++ 10062024/lab01.py
@@ -0,0 +1,45 @@
+#
+# # not operator(boolean)
+# # is_married= True
+# #
+# # print(not is_married)
+# # print( is_married)
+#
+# a =5
+# b=6
+# c=False
+# print()
+#
+# # arithmetic_operations
+# a =180
+# b= 90
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)# float
+# # MOdulus operator-it always give reminder
+# a =87
+# b= 10
+# print(a%b)
+# a=10
+# b-20
+# print(a**2)#square root, power root
+#
+#
+# #logical operators
+# x=10
+# y=20
+# print(x>y)
+# print(x<y)
+
+a=10
+b=10
+print(a>=b) # 10> 10 or 10=10
+print(a==b)# 10>10 or 10=10
+print(not a)
+
+# or Gate
+f=False
+t= True
+print(f and t)
+print(f or t)
---
+++ 10062024/lab02.py
@@ -0,0 +1,26 @@
+import math
+
+x = 10
+y = 20
+result = (x != y)  #10 not equal to 20 -->True
+print(result)
+
+# Ternary operator
+pramod_marks = 90
+amit_marks = 100
+#  x>y --> do some thing called print("pramod")
+#y>x--> do some thing else called print("pramod")
+
+print("pramod is winner" if pramod_marks > amit_marks else "amit is winner")
+
+#progmarm-calculte the area of the circle
+#input-->radius
+#ourput--> area of the circle
+radius = float(input("Enter the radius of the circle \n"))
+print(math.pi)
+# area= math.pi*(radius**2)
+# area2=math.pi*(math.pow(radius,__y,2)))
+
+
+#Math
+import math
